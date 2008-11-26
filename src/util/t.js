4b4eb2312106afd031a986ceb346f8f8ab8eae7c
ssshanest <ssshanest@1f608a5a-b5fc-11dd-8584-61fb05dc49a7>
2008-11-26 06:02:59 +0000

    =Removed proof of concepts from repository; will include in completed test suite
    git-svn-id: http://mathnetics.googlecode.com/svn/trunk@5 1f608a5a-b5fc-11dd-8584-61fb05dc49a7

--- src/util/t.js
+++
@@ -1,9 +0,0 @@
-(function() {
-function getHi() { 
-	alert(mathnetics.util.select("#1234"));
-	alert(mathnetics.util.select(".5678"));
-	alert(mathnetics.util.hasClass(mathnetics.util.select(".5678")[0],"5678"));
-}
-mathnetics.util.onDOMReady(getHi);
-alert(mathnetics.util.Element.Line(1,1,1,1).toHTML().getAttribute("x1"));
-})();
